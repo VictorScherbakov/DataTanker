53334b5666c788301ecd6bd8b2fcdf458fd71a98
VictorScherbakov <headless@land.ru>
2016-11-28 20:21:43 +0300

    Add missing members to IStorageFactory

--- DataTanker/Core/IStorageFactory.cs
+++ DataTanker/Core/IStorageFactory.cs
@@ -1,7 +1,6 @@
 ﻿namespace DataTanker
 {
     using System;
-
     using Settings;
 
     /// <summary>
@@ -9,6 +8,101 @@
     /// </summary>
     public interface IStorageFactory
     {
+        /// <summary>
+        /// Creates a new bytearray storage instance with specified type of keys using built-in serialization routines for keys.
+        /// Supported types of keys are: int, long, uint, ulong, double, float, DateTime, Guid, string and byte[]
+        /// </summary>
+        /// <typeparam name="TKey">The type of key</typeparam>
+        /// <param name="settings">Setiings of creating storage</param>
+        /// <returns>New storage instance</returns>
+        IKeyValueStorage<ComparableKeyOf<TKey>, ValueOf<byte[]>> CreateBPlusTreeByteArrayStorage<TKey>(BPlusTreeStorageSettings settings)
+            where TKey : IComparable;
+
+        /// <summary>
+        /// Creates a new storage instance with specified type of keys and values using built-in serialization routines for keys.
+        /// Supported types of keys are: int, long, uint, ulong, double, float, DateTime, Guid, string and byte[]
+        /// </summary>
+        /// <typeparam name="TKey">The type of key</typeparam>
+        /// <typeparam name="TValue">The type of value</typeparam>
+        /// <param name="valueSerializer">Object implementing ISerializer interface for value serialization</param>
+        /// <param name="settings">Setiings of creraating storage</param>
+        /// <returns>New storage instance</returns>
+        IKeyValueStorage<ComparableKeyOf<TKey>, ValueOf<TValue>> CreateBPlusTreeStorage<TKey, TValue>(ISerializer<TValue> valueSerializer, 
+                                                                                                      BPlusTreeStorageSettings settings)
+            where TKey : IComparable;
+
+        /// <summary>
+        /// Creates a new storage instance with specified type of keys and values using built-in serialization routines for keys
+        /// and BinaryFormatter serialization for values.
+        /// Supported types of keys are: int, long, uint, ulong, double, float, DateTime, Guid, string and byte[]
+        /// </summary>
+        /// <typeparam name="TKey">The type of key</typeparam>
+        /// <typeparam name="TValue">The type of value</typeparam>
+        /// <param name="settings">Setiings of creraating storage</param>
+        /// <returns>New storage instance</returns>
+        IKeyValueStorage<ComparableKeyOf<TKey>, ValueOf<TValue>> CreateBPlusTreeStorage<TKey, TValue>(BPlusTreeStorageSettings settings)
+            where TKey : IComparable;
+
+        /// <summary>
+        /// Creates a new storage instance with specified type of keys using built-in serialization routines.
+        /// Supported types of keys are: int, long, uint, ulong, double, float, DateTime, Guid, string and byte[]
+        /// </summary>
+        /// <typeparam name="TKey">The type of key</typeparam>
+        /// <typeparam name="TValue">The type of value</typeparam>
+        /// <param name="serializeValue">Value serialization method</param>
+        /// <param name="deserializeValue">Value deserialization method</param>
+        /// <param name="settings">Setiings of creraating storage</param>
+        /// <returns>New storage instance</returns>
+        IKeyValueStorage<ComparableKeyOf<TKey>, ValueOf<TValue>> CreateBPlusTreeStorage<TKey, TValue>(Func<TValue, byte[]> serializeValue,
+                                                                                                      Func<byte[], TValue> deserializeValue,
+                                                                                                      BPlusTreeStorageSettings settings)
+            where TKey : IComparable;
+
+        /// <summary>
+        /// Creates a new storage instance with specified types of keys using built-in serialization routines.
+        /// Supported types of keys are: int, long, uint, ulong, double, float, DateTime, Guid, string and byte[]
+        /// </summary>
+        /// <typeparam name="TKey">The type of key</typeparam>
+        /// <typeparam name="TValue">The type of value</typeparam>
+        /// <param name="serializeValue">Value serialization method</param>
+        /// <param name="deserializeValue">Value deserialization method</param>
+        /// <param name="settings">Setiings of creating storage</param>
+        /// <returns>New storage instance</returns>
+        IKeyValueStorage<KeyOf<TKey>, ValueOf<TValue>> CreateRadixTreeStorage<TKey, TValue>(Func<TValue, byte[]> serializeValue,
+                                                                                            Func<byte[], TValue> deserializeValue,
+                                                                                            RadixTreeStorageSettings settings);
+
+        /// <summary>
+        /// Creates a new storage instance with specified types of keys using built-in serialization routines.
+        /// Supported types of keys are: int, long, uint, ulong, double, float, DateTime, Guid, string and byte[]
+        /// </summary>
+        /// <typeparam name="TKey">The type of key</typeparam>
+        /// <typeparam name="TValue">The type of value</typeparam>
+        /// <param name="valueSerializer">Object implementing ISerializer interface for value serialization</param>
+        /// <param name="settings">Setiings of creating storage</param>
+        /// <returns>New storage instance</returns>
+        IKeyValueStorage<KeyOf<TKey>, ValueOf<TValue>> CreateRadixTreeStorage<TKey, TValue>(ISerializer<TValue> valueSerializer, RadixTreeStorageSettings settings);
+
+        /// <summary>
+        /// Creates a new storage instance with specified types of keys using built-in serialization routines.
+        /// and BinaryFormatter serialization for values.
+        /// Supported types of keys are: int, long, uint, ulong, double, float, DateTime, Guid, string and byte[]
+        /// </summary>
+        /// <typeparam name="TKey">The type of key</typeparam>
+        /// <typeparam name="TValue">The type of value</typeparam>
+        /// <param name="settings">Setiings of creating storage</param>
+        /// <returns>New storage instance</returns>
+        IKeyValueStorage<KeyOf<TKey>, ValueOf<TValue>> CreateRadixTreeStorage<TKey, TValue>(RadixTreeStorageSettings settings);
+
+        /// <summary>
+        /// Creates a new bytearray storage instance with specified type of keys using built-in serialization routines.
+        /// Supported types of keys are: int, long, uint, ulong, double, float, DateTime, Guid, string and byte[]
+        /// </summary>
+        /// <typeparam name="TKey">The type of key</typeparam>
+        /// <param name="settings">Setiings of creating storage</param>
+        /// <returns>New storage instance</returns>
+        IKeyValueStorage<KeyOf<TKey>, ValueOf<byte[]>> CreateRadixTreeByteArrayStorage<TKey>(RadixTreeStorageSettings settings);
+
         /// <summary>
         /// Creates a new storage instance with specified types of keys and values.
         /// </summary>
