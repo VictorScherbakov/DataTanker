e8ae7ce7813d170b4398080cfb395d04536c4222
matteofabbri <matteo@phascode.org>
2018-11-20 13:42:39 +0100

    removed redundant methods

--- DataTanker/Core/BinaryFormat/Page/ItemsPageHeader.cs
+++ DataTanker/Core/BinaryFormat/Page/ItemsPageHeader.cs
@@ -1,17 +1,6 @@
 namespace DataTanker.BinaryFormat.Page
 {
-    using PageManagement;
-
     internal abstract class ItemsPageHeader : PageHeaderBase
     {
-        public override void Read(IPage page)
-        {
-            base.Read(page);
-        }
-
-        public override void WriteToPage(IPage page)
-        {
-            base.WriteToPage(page);
-        }
     }
 }
