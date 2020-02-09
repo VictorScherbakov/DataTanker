39f2da114e9c905a63b1a9c0cf22e2e78d2c18ed
mario206 <5844092+mario206@users.noreply.github.com>
2020-02-09 17:47:12 +0800

    [bugfix] WriteMultipageItemBlock
    when the rawLength is 2772,it'a fake multipageItem as it can be stored in one page.

--- DataTanker/Core/BinaryFormat/Page/PageFormatter.Multipage.cs
+++ DataTanker/Core/BinaryFormat/Page/PageFormatter.Multipage.cs
@@ -29,7 +29,8 @@
             if (item.GetAllocationType(page.Length) == AllocationType.SinglePage)
                 throw new PageFormatException("Unable to add item block on page. Item allocation type is single page.");
 
-            int last = Math.Min(page.Length - header.Length, (int)(item.RawData.LongLength - offset)) - 1;
+            // when the rawLength is 2772,it'a fake multipageItem as it can be stored in one page.
+            int last = Math.Min(page.Length - header.Length - (offset == 0 ? 8 : 0), (int)(item.RawData.LongLength - offset)) - 1;
             int contentOffset = header.Length;
 
             if (offset == 0)
@@ -37,8 +38,6 @@
                 // we should write the length of the object
                 var lengthBytes = BitConverter.GetBytes(item.RawData.LongLength);
                 lengthBytes.CopyTo(page.Content, header.Length);
-
-                last -= 8;
                 contentOffset += 8;
             }
 
@@ -78,9 +77,6 @@
             if (header.PreviousPageIndex == -1)
             {
                 offset += 8;
-                // corresponding to the ` last -= 8 ` in function `WriteMultipageItemBlock`.
-                // otherwise,the last 8 bytes will be eaten!
-                length -= 8;  
             }
 
             return ReadBlock(page, Math.Min(length, page.Length - offset), offset);
